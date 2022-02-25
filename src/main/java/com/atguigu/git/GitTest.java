6d6c1aa836e8c0c9d2bfb171638678fc308f1f0f
renbiqian <844615849@qq.com>
2022-02-25 11:41:27 +0800

    hot-fix commit1

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -10,6 +10,7 @@ public class GitTest {
         System.out.println("gitTest01");
         System.out.println("gitTest02");
         System.out.println("gitTest03");
+        System.out.println("hotfixTest");
     }
 
 }
