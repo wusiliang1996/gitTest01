0802afb782887d656de9f344d4d2fdf41bf0cc15
renbiqian <844615849@qq.com>
2022-02-25 11:24:39 +0800

    third commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -9,6 +9,7 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("gitTest01");
         System.out.println("gitTest02");
+        System.out.println("gitTest03");
     }
 
 }
