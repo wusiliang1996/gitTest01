498ae690efbd78b2d55967cbd701377d2b4f967b
renbiqian <844615849@qq.com>
2022-02-25 11:12:34 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,13 @@
+package com.atguigu.git;
+
+/**
+ * @author renbiqian
+ * @create 2022-02-25 11:10
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("gitTest01");
+    }
+
+}
